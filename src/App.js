50e2a49d1d0a74686cc4af7349758016ec81a2a5
bikuu <bikuu@github.com>
2023-06-06 16:40:09 +0545

    created view profile layout

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import SearchBar from "./components/searchBar/SearchBar";
 import Footer from "./components/footer/Footer";
+import ViewProfile from "./components/viewProfile/ViewProfile";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Banner />
+      {/* <Banner />
       <SearchBar />
-      <Home />
+      <Home /> */}
+      <ViewProfile />
       <Footer />
     </div>
   );

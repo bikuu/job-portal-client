9023d394b4bd5e2c9cc47cddfb2eee7c8c83c2b7
bikuu <bikuu@github.com>
2023-06-13 17:07:17 +0545

    started editing profile

--- src/components/editProfile/EditProfile.jsx
+++ src/components/editProfile/EditProfile.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const EditProfile = () => {
-  return <div>EditProfile</div>;
+  return (
+    <div className="container">
+      <div className="leftContainer">Main Container</div>
+      <div className="content">Content</div>
+    </div>
+  );
 };
 
 export default EditProfile;
